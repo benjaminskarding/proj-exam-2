3a9a9c3d9e4b7ea20ba95b79ba2c7f0fcb2c43d1
benjaminskarding <benjamin.skarding@hotmail.com>
2025-04-16 15:52:43 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-red-500 text-white flex items-center justify-center">
+      <h1 className="text-5xl font-extrabold underline">TAILWIND. IS. ON.</h1>
+    </div>
+  );
+}
+
+export default App;
